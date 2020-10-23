7a116708c2dc3a818ebe883d3123fea2bc283129
Julie P <paupertjulie@gmail.com>
2020-10-23 20:55:28 +0200

    gameboard class

---
+++ GameBoard.js
@@ -0,0 +1,68 @@
+import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from './setup';
+
+class GameBoard {
+    constructor(DOMGrid) {
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid = DOMGrid;
+    };
+
+    showGameStatus(gameWin) {
+        //Display if game is over or won 
+        //Create a div and attach it to gameboard
+        const div = document.createElement('div');
+        div.classList.add('game-status');
+        div.innerHTML = `${gameWin ? 'GG !' : 'GAME OVER !'}`;
+        this.DOMGrid.appendChild(div);
+    };
+
+    createGrid(level) {
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid.innerHTML = '';
+        this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px);`;
+
+        //Loop throught each elements with our level 
+        //Create new div on each element
+        //Set the classList to a square
+        //Apply a class depending on the value of each square
+        //Apply styling to set cell size on each element 
+        level.forEach(square => {
+            const div = document.createElement('div');
+            div.classList.add('square', CLASS_LIST[square]);
+            div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px;`;
+            this.DOMGrid.appendChild(div);
+            this.grid.push(div);
+
+            if (CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
+        });
+    };
+
+    addObject(pos, classes) {
+        this.grid[pos].classList.add(...classes);
+    };
+
+    removeObject(pos, classes) {
+        this.grid[pos].classList.remove(...classes);
+    };
+
+    objectExist(pos, object) {
+        return this.grid[pos].classList.contains(object);
+    };
+
+    //Method to rotate Pacman on the grid
+    rotateDiv(pos, deg) {
+        this.grid[pos].style.transform = `rotate(${deg}deg)`;
+    };
+
+    //Static method
+    static createGameBoard(DOMGrid, level) {
+        const board = new this(DOMGrid);
+        board.createGrid(level);
+        return board;
+    };
+
+    //Method to move pacman
+};
+
+export default GameBoard;
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from '/setup';
+//Classes
+import GameBoard from './GameBoard';
+
+//DOM Elements
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
+
+//Game constants
+const POWER_PILL_TIME = 10000; //ms
+const GLOBAL_SPEED = 80; //ms (global speed for the game loop)
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+//Inital Setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false; //When Pacman eats a power pill
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {
+
+};
+
+function checkCollision(pacman, ghosts) {
+
+};
+
+function gameLoop(pacman, ghosts) {
+
+};
+
+function startGame() {
+
+};
